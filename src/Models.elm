e07f3dfa84375e499cc8bff0b057340ddb39dad7
bakuzan <imannoyed4days@hotmail.co.uk>
2017-09-22 11:12:07 +0100

    Work on task model.

--- src/Models.elm
+++ src/Models.elm
@@ -24,4 +24,8 @@ type alias Task =
   { id: Int
   , description: String
   , isComplete: Bool
+  -- , repeatFrequency: Int // Enum for 0 None, 1 Daily, 2 Weekly, 3 Monthly, 4 Quarterly, 5 Yearly
+  -- , reapatDay: Date // dayOfWeek for weekly, Date for monthly/Quarterly/Yearly
+  -- , createdDate: Date
+  -- , updatedDate: Date
   }
